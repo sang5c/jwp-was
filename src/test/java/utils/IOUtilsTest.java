d0a96a9eb51f6ee4ed1a3d9c18b38f70d77a400b
sang5c <skah321@gmail.com>
2022-08-17 22:36:47 +0900

    feat(IOUtils): BufferedReader를 받아 문자열 리스트를 반환한다.

--- src/test/java/utils/IOUtilsTest.java
+++ src/test/java/utils/IOUtilsTest.java
@@ -6,6 +6,9 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class IOUtilsTest {
     private static final Logger logger = LoggerFactory.getLogger(IOUtilsTest.class);
@@ -17,5 +20,17 @@ public class IOUtilsTest {
         BufferedReader br = new BufferedReader(sr);
 
         logger.debug("parse body : {}", IOUtils.readData(br, data.length()));
+    }
+
+    @Test
+    public void toStringList() throws Exception {
+        String data = "abcd123\r\n" +
+                "qwer\r\n" +
+                "zxcv\r\n";
+        StringReader sr = new StringReader(data);
+        BufferedReader br = new BufferedReader(sr);
+
+        List<String> strings = IOUtils.toStringList(br);
+        assertThat(strings).containsExactly("abcd123", "qwer", "zxcv");
     }
 }
